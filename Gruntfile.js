30fd92d772d8ac748a2502438576b8d456724958
Cole Furfaro-Strode <cfurfarostrode@Coles-MacBook-Pro.local>
2015-10-31 21:02:30 -0400

    grunt, now with gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+
+
+module.exports = function(grunt) {
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  require('time-grunt')(grunt);
+
+  grunt.initConfig({
+    jscs: {
+      src: [
+        'resources/**/*.js',
+        'lib/**/*.js',
+        'test/func/**/*.js',
+        'test/unit/**/*.js',
+        '*.js'
+      ]
+    },
+    jshint: {
+      files: [
+        'test/unit',
+        'test/func',
+        'resources',
+        'lib',
+        '*.js'
+      ],
+      options: {
+        jshintrc: './.jshintrc',
+        ignores: ['Gruntfile.js']
+      }
+    }
+  });
+
+  grunt.registerTask('lint', [
+    'jshint',
+    'jscs'
+  ]);
+};
